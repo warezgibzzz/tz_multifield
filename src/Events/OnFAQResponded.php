74ff7a87ccdc753beb564f495464d61a7b41c45f
Pavel Poronko <me@gibzzz.tk>
2016-09-02 11:52:04 +0700

    Mail send fix

--- src/Events/OnFAQResponded.php
+++ src/Events/OnFAQResponded.php
@@ -23,13 +23,11 @@ class OnFAQResponded
         if (strlen($item['DETAIL_TEXT']) > 0 && $item['IBLOCK_ID'] == 4) {
             $res = MailEvent::send([
                 'EVENT_NAME' => 'FAQ_REPLIED',
-                'MESSAGE_ID' => 75,
                 'C_FIELDS' => [
                     'EMAIL_TO' => $itemEmail['VALUE'],
-                    'MESSAGE_ID' => 75
                 ],
                 'DUPLICATE' => 'N',
-                'LID' => 'ru'
+                'LID' => is_array(SITE_ID)? implode(",", SITE_ID): SITE_ID
             ]);
             if ($res->isSuccess()) {
                 AddMessage2Log('Mail sent');
